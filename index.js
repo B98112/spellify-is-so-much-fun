e228b46523449577342d8304bc3b229edc701c42
B98112 <ben.retzler@gmail.com>
2025-07-20 20:20:24 -0400

    save

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 fetchCard = async (e) => {
   // Getting the Scryfall data.
   const response = await fetch(`https://api.scryfall.com/cards/random?q-t=basic lang=en`);
@@ -6,5 +5,7 @@ fetchCard = async (e) => {
 
   // Updating the card with the image data.
   const imageEl = document.getElementById('card');
+  const cmcEl = document.getElementById('cmc');
   imageEl.src = card.image_uris.normal;
+  cmcEl.src = card.cmc.normal;
 }
