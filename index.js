32028e066fda998b05c138636c60807d73ede856
B98112 <ben.retzler@gmail.com>
2025-07-20 21:48:20 -0400

    save

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 
-fetchCard = async (e) => {
-  // Getting the Scryfall data.
-  const response = await fetch(`https://api.scryfall.com/cards/random`);
-  const card = await response.json();
+const response = await fetch(`https://api.scryfall.com/cards/random`);
+const card = await response.json();
 
-  // Updating the card with the image data.
-  //const imageEl = document.getElementById('card');
-  //const oraTextEl = document.getElementById('oracle');
-  //const nameEl = document.getElementById('cardName');
-  //const typeEl = document.getElementById('typeLine');
-  //const manaEl = document.getElementById('mana');
-  const words = card.name + card.mana_cost + card.type_line + card.oracle_text;
-  //imageEl.src = card.image_uris.normal;
-  //oraTextEl.innerText = card.oracle_text;
-  //nameEl.innerText = card.name;
-  //typeEl.innerText = card.type_line;
-  //manaEl.innerText = card.mana_cost;
-}
+const words = card.name + card.mana_cost + card.type_line + card.oracle_text;
 const secretWord = words.toUpperCase(); // Can be replaced with any word
 
 const wordDiv = document.getElementById('word');
